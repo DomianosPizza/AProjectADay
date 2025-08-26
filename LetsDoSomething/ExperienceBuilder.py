cb098c568b2d69c8d316ac4b44673d6ef1879a0e
Domianospizza <damianl.official@gmail.com>
2025-08-26 16:47:28 -0700

    Made MVP of app
    added to do notes for future feature of app

---
+++ LetsDoSomething/ExperienceBuilder.py
@@ -0,0 +1,9 @@
+import random
+
+
+def build_experience():
+    rand = random.randint(a=0, b=1)
+    if rand == 0:
+        experience = [""]
+    if rand == 1:
+        experience = [""]
--- LetsDoSomething/LetsDoSomethingUI/ds_ui.py
+++ LetsDoSomething/LetsDoSomethingUI/ds_ui.py
@@ -2,12 +2,18 @@ import tkinter as tk
 import random
 
 
-def btnClick_click():
-    global x
-    print('button was clicked')
-    lblInfo.config(text=f'BUTTON HAS BEEN CLICKED:{x} TIMES')
-    x += 1
+def btnClick_indoor():
+    experience = ["To the movies", "Bowling", "Bouldering", "Board Games", "Trading Card Games"]
+    random.shuffle(experience)
+    txtEntry.config(text=f"Let's go {experience[0]}")
 
+def btnClick_outdoor():
+    # TO DO
+    # Read From a JSON file of list of things to do outdoor/indoor, randomly select from there
+    # Take result and search internet for nearby places to do said things
+    experience = ["Hike", "Golf", "To The Gun Range", "To The Mall", "To The Beach"]
+    random.shuffle(experience)
+    txtEntry.config(text=f"Let's go {experience[0]}")
 
 # This is purely for the UI box
 form = tk.Tk()
@@ -17,15 +23,16 @@ form.resizable(False, False)
 
 # Content of UI
 lblMainTitle = tk.Label(form, text="Let's Do Something!")
-lblMainTitle.pack()
 
-lblInfo = tk.Label(form, text="Text Output")
-lblInfo.pack()
+btnclick = tk.Button(form, text="Find Indoor Activity", command=btnClick_indoor)
+btnclick.place(x=87.5, y=200)
 
-btnclick = tk.Button(form, text="Click me", command=btnClick_click)
-btnclick.pack()
 
-txtEntry = tk.Entry(form)
-txtEntry.pack()
+btnclick = tk.Button(form, text="Find Outdoor Activity", command=btnClick_outdoor)
+btnclick.place(x=275, y=200)
+
+txtEntry = tk.Label(form, text="")
+txtEntry.grid(row=3, column=3)
+txtEntry.place(x=175, y=250)
 
 form.mainloop()
